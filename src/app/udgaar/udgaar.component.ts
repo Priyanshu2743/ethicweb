da88eefedec8441e80097f29aa60f6948d0532ad
Priyanshu Mittal <pmktl1234a@gmail.com>
2024-08-14 22:13:32 +0530

    Updated Address, Udgaar Registration Page,

--- src/app/udgaar/udgaar.component.ts
+++ src/app/udgaar/udgaar.component.ts
@@ -1,15 +1,19 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-udgaar',
+  selector: 'app-login',
   templateUrl: './udgaar.component.html',
   styleUrls: ['./udgaar.component.css']
 })
 export class UdgaarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loadLogo();
   }
 
-}
+  loadLogo() {
+  }
+
+  onSubmit(): void {
+  }  
+}  
