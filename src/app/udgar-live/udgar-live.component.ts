fcf3e08e59b3a504dd26198fc14cdddc88baf874
Priyanshu Mittal <pmktl1234a@gmail.com>
2024-09-28 13:42:54 +0530

    Latest Updated

--- src/app/udgar-live/udgar-live.component.ts
+++ src/app/udgar-live/udgar-live.component.ts
@@ -8,9 +8,36 @@ import { Component, OnInit, AfterViewInit  } from '@angular/core';
 export class UdgarLiveComponent implements OnInit {
 
   constructor() { }
+  activeIndex = 0;
 
   ngOnInit() {
   }
 
+  // Tabs Data (Images)
+  tabs = [
+    { imgSrc: 'https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-01-179x179.png' },
+    { imgSrc: 'https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-02-306x306.png' },
+    { imgSrc: 'https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-03-179x179.png' }
+  ];
+
+  // Testimonials Data
+  quotes = [
+    {
+      text: 'I have tried a lot of food delivery services but Plate is something out of this world! Their food is really healthy and it tastes great, which is why I recommend this company to all my friends!',
+      author: 'Peter Lee'
+    },
+    {
+      text: 'Another testimonial text for the second quote. Their services are great!',
+      author: 'John Doe'
+    },
+    {
+      text: 'Third testimonial here, this is fantastic!',
+      author: 'Jane Smith'
+    }
+  ];
+
+  onTabClick(index: number) {
+    this.activeIndex = index;
+  }
 }
 
